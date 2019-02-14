6ad91add7ab47c4cf5ef668cae96d8e12040f398
James Mac William <jimmy.macwilliam@gmail.com>
2019-02-14 13:23:34 -0500

    count combust to day5

--- day5/day5.rb
+++ day5/day5.rb
@@ -5,14 +5,16 @@ reg = []
 ('a'..'z').to_a.each { |c| reg << "(#{c}#{c.capitalize})|(#{c.capitalize}#{c})" }
 reg = /#{reg.join('|')}/
 
-def combust(str, reg)
+def combust(str, reg, i = 0)
   temp = str.gsub(reg, '')
-  return combust(temp, reg) if temp.size < str.size
+  i += 1
+  return combust(temp, reg, i) if temp.size < str.size
+  puts i
   return temp
 end
 
 puts combust(input, reg).size
 
-('a'..'z').to_a.each do |c|
-  puts "#{c}: #{combust(input.gsub(/#{c}/i, ''), reg).size}"
-end
+#('a'..'z').to_a.each do |c|
+  #puts "#{c}: #{combust(input.gsub(/#{c}/i, ''), reg).size}"
+#end
